39379825f129ed8e8d6f681f28dc0050682fa334
MrSixtorz <sixto723@gmail.com>
2023-11-07 19:35:45 +0100

    Solucionado problemas con compileSdk y con la imagen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.linearlayout_02"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.linearlayout_02"
